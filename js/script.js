e2f4230342444cf0e2279b979507da700670bd36
alondrajjaur <alondra.j.2003@gmail.com>
2019-10-31 01:13:59 +0000

    javas

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
-
+$("document").ready(function(){
+$("button").click(function () {
+    var name =$("#input").val();
+    $("#userChoice").append(name)
+})
+});
 //GLOBAL VARIABLES
 /* global $ */
 
