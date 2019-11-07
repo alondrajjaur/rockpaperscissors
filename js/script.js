7e658a1572988eaa19843f666967df390ccf7c37
alondrajjaur <42780617+alondrajjaur@users.noreply.github.com>
2019-11-06 18:26:52 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
+
 $("document").ready(function(){
 $("button").click(function () {
     var name =$("#input").val();
-    $("#userChoice").append(name)
+    $("#userChoice").html(name)
+  let Computerchoice = Math.random();
+  console.log(Computerchoice);
+    if (Computerchoice < .333) {
+        $("#result").html("rock");
+    } else if(Computerchoice > .7) {
+      $("#result").html("paper")
+    } else {
+      $("#result").html("scissors")
+    }
 })
 });
+
 //GLOBAL VARIABLES
 /* global $ */
 
